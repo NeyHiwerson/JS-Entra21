05b9758777150c45a35f95567663ff9ff47da10f
Ney Hiwerson <hiwerson@gmail.com>
2023-12-07 20:21:50 -0300

    finalizado exercicio da aula05

--- aula05/assets/js/temporizador.js
+++ aula05/assets/js/temporizador.js
@@ -1,31 +1,64 @@
-
 let contagem = 0;
+let controleContagem = true;
+let contvoltas = 1;
 
-const temporizador1 = document.getElementById('temporizador1');
+const temporizador1 = document.getElementById("temporizador1");
+const informeUsuario = document.getElementById("informe-usuario");
 
-const contador = function(){
+const contador = function () {
   contagem++;
   temporizador1.innerHTML = `<h2>${contagem}</h2>`;
-}
+};
 
 let repita1 = setInterval(contador, 1000);
 
 const pararTemporizador1 = (elemento) => {
   clearInterval(repita1);
-  repita1 = null;
-}
+  controleContagem = false;
+  //informar ao usuário demais instruçoes
+  informeUsuario.innerHTML = `
+    <h4>Para continuar clique em continuar</h4>
+    <h4>Para reiniciar clique em reiniciar</h4>
+    `;
+  /* função para apagar instruções após 3 segundos */
+  setTimeout(() => {
+    informeUsuario.innerHTML = "";
+  }, 3000);
+};
 
 const continuarTemporizador1 = () => {
-  if (repita1 == null) {
+  if (!controleContagem) {
     repita1 = setInterval(contador, 1000);
+
+    //informar ao usuário demais instruçoes
+    informeUsuario.innerHTML = `
+      <h4>Para Parar clique em Parar</h4>
+      <h4>Para reiniciar clique em reiniciar</h4>
+      `;
+    controleContagem = true;
+    /* função para apagar instruções após 3 segundos */
+    setTimeout(() => {
+      informeUsuario.innerHTML = "";
+    }, 3000);
   }
 };
 
-let testTimeout = setTimeout(() => {
-  alert('O tempo acabou!');
-}, 3000);
-
-const pararTestTimeout = setTimeout(() => {
-  clearTimeout(testTimeout);
-  testTimeout = null;
-});
+const reiniciarTemporizador1 = () => {
+  if(controleContagem == false){
+    clearInterval(repita1);
+    contagem = 0;
+    repita1 = setInterval(contador, 1000);
+    informeUsuario.innerHTML = `
+      <h4>Para Parar clique em Parar</h4>
+      <h4>Para reiniciar clique em reiniciar</h4>
+      `;
+    controleContagem = true;
+    setTimeout(() => {
+      informeUsuario.innerHTML = "";
+    }, 3000);
+  }else{
+    informeUsuario.innerHTML += `<h4>Time ${contvoltas}: ${contagem} segundos.</h4>`;
+    contagem = 0;
+    contvoltas++;
+  }  
+};
